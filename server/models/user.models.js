a9cc6c8f316072c4e2b5d372a86e0baf4819b207
Sudesh Kumar Santhosh Kumar <santhosh@usc.edu>
2023-12-06 16:48:35 -0800

    Updated user model with compatibilty score

--- server/models/user.models.js
+++ server/models/user.models.js
@@ -1,54 +1,70 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema(
-    {
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        profilePicture: {
-            type: String,
-            default:
-                'https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg',
-        },
-        isSpotifyConnected: {
-            type: Boolean,
-            default: false,
-        },
-        spotifyAccessToken: {
-            type: String,
-            default: null
-        },
-        spotifyRefreshToken: {
-            type: String,
-            default: null
-        },
-        spotifyTokenExpiry: {
-            type: Date,
-            default: null
-        },
-        spotifyGenres: {
-            type: [String], // Favorite genres
-            default: []
-        },
-        spotifyArtists: {
-            type: [String], // Favorite artists
-            default: []
-        },
+// No need for separate genreSchema since it's only a string
+// No need for separate artistSchema and trackSchema since they are simple objects and not used elsewhere
 
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
     },
-    { timestamps: true }
-);
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    profilePicture: {
+        type: String,
+        default: 'https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg',
+    },
+    isSpotifyConnected: {
+        type: Boolean,
+        default: false,
+    },
+    spotifyAccessToken: {
+        type: String,
+        default: null
+    },
+    spotifyRefreshToken: {
+        type: String,
+        default: null
+    },
+    spotifyTokenExpiry: {
+        type: Date,
+        default: null
+    },
+    spotifyGenres: {
+        type: [String], // Array of genres as strings
+        default: [],
+    },
+    spotifyArtists: {
+        type: [{
+            name: String,
+            albumImageUrl: String,
+        }],
+        default: [], // Explicitly set an empty array as the default value
+    },
+    spotifyTracks: {
+        type: [{
+            name: String,
+            albumImageUrl: String,
+        }],
+        default: [], // Explicitly set an empty array as the default value
+    },
+
+    compatibilityScores: {
+        type: [{
+            user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+            score: Number,
+        }],
+        default: [] 
+    }
+}, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
 
