f845b5b5d36fe101f8eeb6108c2c13e7df7434c8
Sudesh Kumar Santhosh Kumar <santhosh@usc.edu>
2023-12-08 07:58:18 -0800

    Showe route added

--- server/routes/listings.routes.js
+++ server/routes/listings.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createListing, getAllListings } from "../controllers/listings.controller.js";
+import { createListing, getAllListings, updateListing, deleteListing } from "../controllers/listings.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
 router.post('/create/:id', verifyToken, createListing);
-router.get('/listings/', verifyToken, getAllListings);
-
+router.get('/show/', verifyToken, getAllListings);
+router.post("/update/:id", verifyToken, updateListing);
+router.delete("/delete/:id", verifyToken, deleteListing);
 export default router; 
