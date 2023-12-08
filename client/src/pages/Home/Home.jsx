dc54af2253b66a14e6e1a21307e696837d79b7f1
Sudesh Kumar Santhosh Kumar <santhosh@usc.edu>
2023-12-08 10:54:31 -0800

    Fixed the listings bugs

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import CircularProgress from '@mui/material/CircularProgress';
+import ListingsCard from "../../components/ListingsCard";
 
 const Home = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  const [listings, setListings] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`/api/listings/show/${currentUser._id}`);
+      const data = await response.json();
+
+      if (data.success === false) {
+        console.log(data.message);
+        setError(data.message);
+        return;
+      }
+      console.log(data);
+      setListings(data);
+    } catch (error) {
+      console.error("Error fetching listings:", error);
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <CircularProgress />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      {listings.map((listing, index) => (
+        <ListingsCard key={index} listing={listing} />
+      ))}
+    </div>
+  );
+};
 
 export default Home;
